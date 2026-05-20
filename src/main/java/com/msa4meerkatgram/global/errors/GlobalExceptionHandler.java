88d5e6330de3c9aa83b14d210a9823176955ca0c
oso7865-ship-it <oso7865@gmail.com>
2026-05-20 16:37:13 +0900

    260519 [develop] 예외 핸들러

---
+++ src/main/java/com/msa4meerkatgram/global/errors/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.msa4meerkatgram.global.errors;
+
+import com.msa4meerkatgram.global.responses.GlobalResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<GlobalResponse<String>> methodArgumentTypeMismatchExceptionHandle(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity.status(400).body(
+            GlobalResponse.<String>builder()
+                .code("E21")
+                .message("요청 파라미터에 이상이 있습니다.")
+                .data(String.format("%s : 필드를 확인해 주세요.",e.getName()))
+                .build()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GlobalResponse<List<String>>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(400).body(
+            GlobalResponse.<List<String>>builder()
+                .code("E21")
+                .message("요청 파라미터에 이상이 있습니다.")
+                .data(
+                        e.getBindingResult()
+                        .getAllErrors()
+                        .stream()
+                        .map(ObjectError::getDefaultMessage)
+                        .toList()
+                )
+                .build()
+        );
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GlobalResponse<String>> othersHandle(Exception e) {
+        log.error(String.format("시스템 에러: %s\n%s",e.getMessage(), Arrays.toString(e.getStackTrace())));
+        return ResponseEntity.status(500).body(
+            GlobalResponse.<String>builder()
+                           .code("E99")
+                           .message("시스템 에러")
+                           .data("현재 서비스 이용이 불가합니다. 잠시후 다시 시도해 주십시오.")
+                           .build()
+        );
+    }
+}
