12750290970c74b6328c8ad558f14e29a7475317
oso7865-ship-it <oso7865@gmail.com>
2026-05-27 13:19:24 +0900

    260519 [develop] Post 페이지네이션 기능

---
+++ src/main/java/com/msa4meerkatgram/domain/user/services/UserService.java
@@ -0,0 +1,12 @@
+package com.msa4meerkatgram.domain.user.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    public String test() {
+
+    }
+}
